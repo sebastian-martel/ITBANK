6551619c6fa720689ed9f8043a19b12652e0627d
Martin Rodriguez <martin.rodriguez@mcplusa.com>
2023-09-01 15:40:10 -0300

    funcionalidad de login

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,11 +3,18 @@ const userpassword = document.getElementById('userpassword')
 const boton = document.getElementById('boton')
 
 
-boton.addEventListener('click', (e) => { 
-    e.preventDefault()
+function validateLogin() {
     const data = {
         useremail: useremail.value,
         userpassword: userpassword.value
     }
-    console.log(data)
+    if (data.useremail === "pepe" && data.userpassword === "123") {
+        window.location.href = "cuentas.html"
+    } else {
+        alert("Usuario y/o contraseña incorrectos.")
+    }
+}
+boton.addEventListener('click', (e) => {
+    e.preventDefault()
+    validateLogin()
 })
